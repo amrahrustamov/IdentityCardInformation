da674a251b9d8b022c1dd7852699167d8739959e
Amrah Rustamov <amrahrustamov94@gmail.com>
2024-07-24 12:06:22 +0400

    database configuration completed and downloaded sql package

---
+++ Database/UserDataDbContext.cs
@@ -0,0 +1,23 @@
+﻿using İdentityCardİnformation.Database.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace İdentityCardİnformation.Database
+{
+    public class UserDataDbContext :DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public UserDataDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var sqlDataSource = _configuration.GetConnectionString("UserDataDbContext");
+            optionsBuilder.UseNpgsql(sqlDataSource);
+
+            base.OnConfiguring(optionsBuilder);
+        }
+        public DbSet<User> users { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using İdentityCardİnformation.Database;
+using Microsoft.EntityFrameworkCore;
+
 namespace İdentityCardİnformation
 {
     public class Program
@@ -7,7 +10,11 @@ namespace İdentityCardİnformation
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            builder.Services
+            .AddDbContext<UserDataDbContext>(o =>
+            {
+                o.UseNpgsql(builder.Configuration.GetConnectionString("UserDataDbContext"), b => b.MigrationsAssembly("UserData"));
+            });
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
